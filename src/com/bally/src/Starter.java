4332803ea1ec5bf1f9d4e45a77a63a886b3d2697
Pankaj Shinde <p10shinde@gmail.com>
2022-03-06 00:20:59 -0500

    Added method docs, assumptions

--- src/com/bally/src/Starter.java
+++ src/com/bally/src/Starter.java
@@ -10,7 +10,16 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
+/**
+ * @author anonymous
+ *
+ */
 public class Starter {
+
+//	Assumptions:
+//	1. Lower limit is 1 and highest random number is a million
+//	2. Numbers need not to be unique
+//	3. All number are positive
 
 	final static public String OUTPUT_DIR = "./output";
 	final static public String OUTPUT_FILE = "/result.txt";
@@ -20,7 +29,7 @@ public class Starter {
 	public static void main(String[] args) throws IOException {
 
 		try {
-			makeDir();
+			makeDir(OUTPUT_DIR);
 
 			Path path = Paths.get(OUTPUT_DIR + "/" + OUTPUT_FILE);
 			Path pathSorted = Paths.get(OUTPUT_DIR + "/" + OUTPUT_FILE_SORTED);
@@ -41,13 +50,25 @@ public class Starter {
 		}
 	}
 
-	public static void makeDir() {
-		File theDir = new File(OUTPUT_DIR);
+	/**
+	 * Creates a directory if does not exist
+	 * 
+	 * @param output_dir A directory which needs to be created
+	 */
+	public static void makeDir(String output_dir) {
+		File theDir = new File(output_dir);
 		if (!theDir.exists()) {
 			theDir.mkdirs();
 		}
 	}
 
+	/**
+	 * Sorts a list of integers and stores the output to a text file
+	 * 
+	 * @param randNums   List of random integers to sort
+	 * @param pathSorted Location of the file to save the output
+	 * @throws IOException Throws IOException
+	 */
 	public static void sortAndWrite(List<Integer> randNums, Path pathSorted) throws IOException {
 		randNums = randNums.stream().sorted(Collections.reverseOrder()).collect((Collectors.toList()));
 		BufferedWriter sortedWriter = Files.newBufferedWriter(pathSorted);
